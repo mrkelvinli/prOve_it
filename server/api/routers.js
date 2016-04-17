d73abc3ab64d917e5f3b9c965cb0948d7e3de02b
Kelvin Li <kelvinlcf@yahoo.com.hk>
2016-04-17 14:48:20 +1000

    upload end point

--- server/api/routers.js
+++ server/api/routers.js
@@ -5,24 +5,22 @@ if (Meteor.isServer) {
 
   Router.onBeforeAction(function(req, res, next) {
     req.start_time = new Date();
-//    console.log(req.headers);
+//    console.log(req);
     var files = []; // Store a file and then pass it to the request.
+    var token = Random.id( 10 );
 
     if (req.method === "POST" && parseInt(req.headers['content-length']) > 0) {
       var busboy = new Busboy({
           headers: req.headers
       });
       busboy.on("file", function(fieldname, file, filename, encoding, mimetype) {
-        // var saveTo = path.join(os.tmpDir(), filename);
-        // var saveTo = path.join(process.cwd()+ "/tmp/", filename);
-        // var token = Random.id( 10 );
-        // req.token = token;
-        // var saveTo = path.join(process.env.PWD, "private", filename);
+        var saveTo = path.join(process.env.PWD, "private",token,filename)
         var fileSizeBytes = 0;
         var contents = '';
 
-        // file.pipe(fs.createWriteStream(saveTo));
-        // var string = Assets.getText(filename);
+        file.pipe(fs.createWriteStream(saveTo));
+        console.log(saveTo);
+//         var string = Assets.getText(filename);
 
         file.on("data", function(data) {
           fileSizeBytes = fileSizeBytes + data.length;
@@ -42,6 +40,7 @@ if (Meteor.isServer) {
       busboy.on("finish", function() {
         // Pass the file on to the request
         // console.log(files);
+        req.token = token;
         req.files = files;
         next();
       });
@@ -53,7 +52,21 @@ if (Meteor.isServer) {
   });
 }
 
-Router.route( '/api/v1/event-study/cumulative_returns', function() {
+Router.route( '/api/v1/event-study/submit-files', function() {
+  this.response.setHeader( 'Access-Control-Allow-Origin', '*' );
+
+  if ( this.request.method === "OPTIONS" ) {
+    this.response.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    this.response.setHeader( 'Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS' );
+    this.response.end( 'Set OPTIONS.' );
+  } else {
+    Uploader.handleRequest( this, this.request.method);
+  }
+}, { where: 'server' } );
+
+
+
+Router.route( '/api/v1/event-study/cumulative-returns', function() {
 
   this.response.setHeader( 'Access-Control-Allow-Origin', '*' );
 
@@ -65,3 +78,11 @@ Router.route( '/api/v1/event-study/cumulative_returns', function() {
     API.handleRequest( this, this.request.method);
   }
 }, { where: 'server' } );
+
+
+Router.route("/api/", function() {
+  this.render('api_index');
+}, { where: 'server' } );
+
+
+
---
+++ server/api/uploader.js
@@ -0,0 +1,91 @@
+Uploader = {
+  connection: function( request ) {
+    var getRequestContents = API.utility.getRequestContents( request );
+    return { data: getRequestContents };
+  },
+
+  handleRequest: function( context, method ) {
+    var connection = API.connection( context.request );
+    if ( !connection.error ) {
+      var params = connection.data;
+      var files  = context.request.files;
+      Uploader.methods[ method ]( context, params, files );
+    } else {
+      API.utility.response( context, 401, connection );
+    }
+  },
+
+  methods: {
+    POST: function( context, params, files ) {
+//      if (!API.utility.hasData(params)){
+//        API.utility.response( context, 404, { error: 404, message: "Invalid Request (no parameters found), dude." } );
+//      }
+
+      var status_code = 200;
+
+      // files parsing
+      for (var id in files) {
+        var parseObject = Papa.parse(files[id]['contents'])
+        files[id]['json'] = parseObject.data;
+        files[id]['errors'] = parseObject.errors;
+        if (files[id]['errors'].length > 0) {
+          API.utility.response( context, 422, {
+            log : API.utility.api_log(params, files, context.request.start_time, "CSV file isn't formatted correctly."),
+          });
+        }
+      }
+
+      // Make sure that our request has data and that the data is valid.
+      var requestOK   = Uploader.utility.validateRequest(files);
+
+      if ( requestOK ) {
+        API.utility.response( context, status_code, {
+            log : API.utility.api_log(params, files, context.request.start_time, "successful"),
+
+        });
+      } else {
+        API.utility.response( context, 404, {
+          log : API.utility.api_log(params, files, context.request.start_time, "Invalid Request."),
+        });
+      }
+      
+      
+    },
+  },
+
+  utility: {
+    getRequestContents: function( request ) {
+      return request.query;
+    },
+    hasData: function( data ) {
+      return Object.keys( data ).length > 0 ? true : false;
+    },
+    response: function( context, statusCode, data ) {
+      context.response.setHeader( 'Content-Type', 'application/json' );
+      context.response.statusCode = statusCode;
+      context.response.end( JSON.stringify( data) );
+    },
+    validate: function( data, pattern ) {
+      return Match.test( data, pattern );
+    },
+    validateRequest: function (files ) {
+      var stock_price_file_OK = false;
+      var stock_characteristic_file_OK = false
+      for (var id in files) {
+        if (files[id]['fieldname'] == "stock_price_file" && !stock_price_file_OK){
+          stock_price_file_OK = true;
+        } else if (files[id]['fieldname'] == "stock_characteristic_file" && !stock_characteristic_file_OK) {
+          stock_characteristic_file_OK = true;
+        }
+      }
+      return files.length == 2 &&
+        stock_price_file_OK &&
+        stock_characteristic_file_OK;
+    },
+  },
+};
+
+
+function isNumeric(num) {
+    return !isNaN(num)
+}
