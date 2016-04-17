b04bd61a0d1e575477356d38c41140da82a44fe9
Kelvin Li <kelvinlcf@yahoo.com.hk>
2016-04-17 23:00:46 +1000

    root url points to gui

--- client/main.js
+++ client/main.js
@@ -1,34 +1,33 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
 import './guiPage.html';
 
-Router.route('/initGui', function () {
-  this.render('gui');
-});
 Router.route('/', function(){
-  this.render('Hello');
+  this.render('api_gui');
+});
+
+Router.configure({
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
 });
 
 
 
-
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+//Template.hello.onCreated(function helloOnCreated() {
+//  // counter starts at 0
+//  this.counter = new ReactiveVar(0);
+//});
+//
+//Template.hello.helpers({
+//  counter() {
+//    return Template.instance().counter.get();
+//  },
+//});
+//
+//Template.hello.events({
+//  'click button'(event, instance) {
+//    // increment the counter when button is clicked
+//    instance.counter.set(instance.counter.get() + 1);
+//  },
+//});
