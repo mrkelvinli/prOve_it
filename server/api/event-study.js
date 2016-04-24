63216aa935a0aeabbe174397eedeb250ce6630da
Yan Chi Ku <duubyplz@hotmail.com>
2016-04-24 17:29:42 +1000

    get_all_events

--- server/api/event-study.js
+++ server/api/event-study.js
@@ -160,6 +160,30 @@ ES = {
     return all_company;
   },
 
+  get_all_events: function(stock_characteristic_file) {
+    var title_line = stock_characteristic_file[0];
+    var line_length = title_line.length();
+    var results = [];
+    for (var i=0; i<stock_characteristic_file.length(); i++) {
+      var current_line = stock_characteristic_file[i];
+      for (var j=2; j<line_length; j++) {
+        if (current_line[j] == 1) {
+          var company_name = title_line[0].toString();
+          var date = title_line[1].toString();
+          var event_type = title_line[j].toString();
+          var value = current_line[j];
+          result.push({
+            company_name: company_name,
+            date: date,
+            event_type: event_type,
+            value: value,
+          });
+        }
+      }
+    }
+    return results;
+  },
+
   calc_cumulative_returns: function (stock_price_file) {
     if (stock_price_file == null) {
       return stock_price_file;
