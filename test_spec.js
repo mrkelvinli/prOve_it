8a3928438719dc4beb3712f7ac1118062e0227d2
Keith <keithlai124@hotmail.com>
2016-05-02 23:20:27 +1000

    finished all cum return checks

--- test_spec.js
+++ test_spec.js
@@ -27,15 +27,13 @@ frisby.create('POST upload files with no input')
 .post('http://prove-it-unsw.herokuapp.com/api/v1/event-study/submit-files',  {
 
   })
-.timeout(10000) // 10 second timeout
+.timeout(100000) // 10 second timeout
 .expectStatus(404)
 .expectHeaderContains('content-type', 'application/json')
 .expectJSON({
   log: {
     team_name: 'prOve it',
     version: 'v1',
-    input_filename: [],
-    parameters_passed: {},
     exec_status: 'No CSV file.'
   }
 })
@@ -46,18 +44,149 @@ frisby.create('POST upload files with no input')
 
 // ---- TESTING CUMULATIVE RETURN ENDPOINT ----
 
-frisby.create('GET data with valid input')
-  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=1&lower_window=-1&topic_name=Cash Rate&topic_upper_range=1.5&topic_lower_range=-1.5&token=diwvTsq7xBGGxGqtFJkT')
-  .timeout(10000) // 10 second timeout
+frisby.create('Send request with valid input')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=Cash Rate&topic_upper_range=1.5&topic_lower_range=-1.5&token=diwvTsq7xBGGxGqtFJkT')
+  .timeout(100000) // 10 second timeout
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
 .toss();
 
-frisby.create('GET data with no input')
+frisby.create('Send request with no input')
   .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns')
-  .timeout(10000) // 10 second timeout
+  .timeout(100000) // 10 second timeout
   .expectStatus(404)
   .expectHeaderContains('content-type', 'application/json')
-  .inspectJSON()
 .toss();
 
+frisby.create('Send request with no token')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=Cash Rate&topic_upper_range=1.5&topic_lower_range=-1.5')
+  .timeout(100000) // 10 second timeout
+  .expectStatus(404)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    log: {
+      team_name: 'prOve it',
+      version: 'v1',
+      input_filename: [],
+      parameters_passed:
+      { upper_window: '5',
+        lower_window: '-5',
+        topic_name: 'Cash Rate',
+        topic_upper_range: '1.5',
+        topic_lower_range: '-1.5' },
+      exec_status: 'Invalid token.'
+    }
+})
+  //.inspectJSON()
+.toss();
+
+frisby.create('Send request with invalid token')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=Cash Rate&topic_upper_range=1.5&topic_lower_range=-1.5&token=ABCDEFG')
+  .timeout(100000) // 10 second timeout
+  .expectStatus(404)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    log: {
+      team_name: 'prOve it',
+      version: 'v1',
+      input_filename: [],
+      parameters_passed:
+      { upper_window: '5',
+        lower_window: '-5',
+        topic_name: 'Cash Rate',
+        topic_upper_range: '1.5',
+        topic_lower_range: '-1.5' },
+      exec_status: 'Invalid token.'
+    }
+})
+  //.inspectJSON()
+.toss();
+
+frisby.create('Send request with invalid variable name')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=DERP&topic_upper_range=1.5&topic_lower_range=-1.5&token=diwvTsq7xBGGxGqtFJkT')
+  .timeout(100000) // 10 second timeout
+  .expectStatus(404)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    log: {
+      team_name: 'prOve it',
+      version: 'v1',
+      input_filename: [],
+      parameters_passed:
+      { upper_window: '5',
+        lower_window: '-5',
+        topic_name: 'DERP',
+        topic_upper_range: '1.5',
+        topic_lower_range: '-1.5' },
+      exec_status: 'Invalid topic name.'
+    }
+})
+  //.inspectJSON()
+.toss();
+
+frisby.create('Send request where upper_window < lower_window')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=-5&lower_window=5&topic_name=Cash Rate&topic_upper_range=1.5&topic_lower_range=-1.5&token=diwvTsq7xBGGxGqtFJkT')
+  .timeout(100000) // 10 second timeout
+  .expectStatus(404)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    log: {
+      team_name: 'prOve it',
+      version: 'v1',
+      input_filename: [],
+      parameters_passed:
+      { upper_window: '-5',
+        lower_window: '5',
+        topic_name: 'Cash Rate',
+        topic_upper_range: '1.5',
+        topic_lower_range: '-1.5' },
+      exec_status: 'Invalid Parameters.'
+    }
+})
+  //.inspectJSON()
+.toss();
+
+frisby.create('Send request where topic_upper_range < topic_lower_range')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=Cash Rate&topic_upper_range=-1.5&topic_lower_range=1.5&token=diwvTsq7xBGGxGqtFJkT')
+  .timeout(100000) // 10 second timeout
+  .expectStatus(404)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    log: {
+      team_name: 'prOve it',
+      version: 'v1',
+      input_filename: [],
+      parameters_passed:
+      { upper_window: '5',
+        lower_window: '-5',
+        topic_name: 'Cash Rate',
+        topic_upper_range: '-1.5',
+        topic_lower_range: '1.5' },
+      exec_status: 'Invalid Parameters.'
+    }
+})
+  //.inspectJSON()
+.toss();
+
+frisby.create('Send request where topic_upper_range < topic_lower_range')
+  .get('http://prove-it-unsw.herokuapp.com/api/v1/event-study/cumulative-returns?upper_window=5&lower_window=-5&topic_name=Cash Rate&topic_upper_range=0.2&topic_lower_range=0.1&token=diwvTsq7xBGGxGqtFJkT')
+  .timeout(100000) // 100 second timeout
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+      log: {
+        team_name: 'prOve it',
+        version: 'v1',
+        input_filename: [],
+        parameters_passed:
+        { upper_window: '5',
+          lower_window: '-5',
+          topic_name: 'Cash Rate',
+          topic_upper_range: '0.2',
+          topic_lower_range: '0.1' },
+        exec_status: 'Successful.'
+      },
+      Event_Cumulative_Return: []
+  })
+//.inspectJSON()
+.toss();
