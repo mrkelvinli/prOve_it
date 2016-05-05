cffc18d93d18eaaf214733de0136133eb8d89448
Kelvin Li <kelvinlcf@yahoo.com.hk>
2016-05-05 23:12:37 +1000

    testing report included

--- wolfofbridgestAPI_spec.js
+++ wolfofbridgestAPI_spec.js
@@ -12,7 +12,7 @@ var token = '5D7F960526BD4246AC6D';
 
 var priceFilePath = path.resolve(__dirname, 'input_files/stock_price.csv');
 var characteristicFilePath = path.resolve(__dirname, 'input_files/event_char.csv');
-/*
+
 var form = new FormData();
 form.append('stock_file', fs.createReadStream(priceFilePath), {
   knownLength: fs.statSync(priceFilePath).size  // we need to set the knownLength so we can call  form.getLengthSync()
@@ -37,9 +37,8 @@ frisby.create('Upload normally')
   //.inspectJSON()
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
-<<<<<<< HEAD
   .toss();
-*/
+
 // -------------------------------------
 
 frisby.create('Send request for cumulative return for China_Growth_Rate_Change and all companies')
@@ -337,84 +336,3 @@ frisby.create('Uploading incorrectly formatted files')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .toss();
-
-// -------------------------------------
-/*
-**********THEY CAN'T PASS THE LAST TWO TESTS************
-
-var priceFilePath = path.resolve(__dirname, 'input_files/empty1.csv');
-var characteristicFilePath = path.resolve(__dirname, 'input_files/empty2.csv');
-var form = new FormData();
-
-form.append('stock_price_file', fs.createReadStream(priceFilePath), {
-  knownLength: fs.statSync(priceFilePath).size  // we need to set the knownLength so we can call  form.getLengthSync()
-});
-
-form.append('stock_characteristic_file', fs.createReadStream(characteristicFilePath), {
-  knownLength: fs.statSync(characteristicFilePath).size
-});
-
-
-frisby.create('Uploading incorrectly formatted files')
-  .post(URL+'api/v1/event-study/submit-files',
-  form,
-  {
-    json: false,
-    headers: {
-      'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
-      'content-length': form.getLengthSync()
-    }
-  })
-  .timeout(100000)
-  .inspectJSON()
-  .expectJSON({
-   log: 
-   { developerTeam: 'Wolf Of Bridge St',
-     errors: 
-      { invalidForm: 
-         { details: 'The data sent was incomplete or incorrect.',
-           name: 'Invalid Form' } },
-     response: 'Error' }
-   })
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .toss();
-// -------------------------------------
-var priceFilePath = path.resolve(__dirname, 'input_files/edited_event_char.csv');
-var characteristicFilePath = path.resolve(__dirname, 'input_files/edited_stock_price.csv');
-var form = new FormData();
-form.append('stock_price_file', fs.createReadStream(priceFilePath), {
-  knownLength: fs.statSync(priceFilePath).size  // we need to set the knownLength so we can call  form.getLengthSync()
-});
-
-form.append('stock_characteristic_file', fs.createReadStream(characteristicFilePath), {
-  knownLength: fs.statSync(characteristicFilePath).size 
-});
-
-
-frisby.create('Uploading incorrectly formatted files')
-  .post(URL+'api/v1/event-study/submit-files',
-  form,
-  {
-    json: false,
-    headers: {
-      'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
-      'content-length': form.getLengthSync()
-    }
-  })
-  .timeout(100000)
-  .inspectJSON()
-  .expectJSON({
-   log: 
-   { developerTeam: 'Wolf Of Bridge St',
-     errors: 
-      { invalidForm: 
-         { details: 'The data sent was incomplete or incorrect.',
-           name: 'Invalid Form' } },
-     response: 'Error' }
-   })
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .toss();
-
-*/
