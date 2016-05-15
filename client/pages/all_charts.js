271f0476b01d5b353d3411a96525e2aa159b5a17
Yan Chi Ku <duubyplz@hotmail.com>
2016-05-15 15:03:38 +1000

    in middle of company graph

--- client/pages/all_charts.js
+++ client/pages/all_charts.js
@@ -10,21 +10,8 @@ Template.chart.rendered = function() {
     console.log(response);
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   render_candlestick_graph(curr_company);
+  render_company_chart();
 
   function render_candlestick_graph (company_name) {
     var chartData = [];
@@ -202,6 +189,131 @@ Template.chart.rendered = function() {
           "enabled": true
         }
       });
+    }
+  }
+
+  function render_company_chart() {
+      // var company_name = "AAC.AX";
+      // var topic = "Cash Rate";
+
+    //   var token = params['token'];
+
+    //   var requestOK = apiCalcCrAvg.utility.validateRequest(token);
+    //   if (requestOK) {
+
+    var file = Files.findOne({
+      token: token
+    });
+    console.log(file);
+    var stock_price_file = file.stock_price_file;
+    var stock_characteristic_file = file.stock_characteristic_file;
+
+
+    var c_cr = [];
+    var all_company = ES.get_all_query_company(stock_price_file);
+    all_company.forEach(function(c){
+
+      var ret = StockPrice.findOne(
+        {
+          company_name: c,
+          token: token,
+        },{
+          fields: {
+            company_name: 1,
+            date: 1,
+            cum_return: 1,
+          },
+          sort:{
+            date:-1,
+          }
+        }
+      );
+      console.log(ret.cum_return);
+      c_cr.push({
+        company_name: c,
+        last_cr: ret.cum_return,
+      });
+    });
+
+    drawGraph(c_cr);
+
+    function handleClick(event) {
+      render_company_topics_chart(event.item.category);
+    }
+
+    function drawGraph(chartData) {
+      // console.log('called');
+
+      /**
+       * AmCharts plugin: automatically color each individual column
+       * -----------------------------------------------------------
+       * Will apply to graphs that have autoColor: true set
+       */
+      AmCharts.addInitHandler(function (chart) {
+        // check if there are graphs with autoColor: true set
+        for (var i = 0; i < chart.graphs.length; i++) {
+          var graph = chart.graphs[i];
+          if (graph.autoColor !== true)
+            continue;
+          var colorKey = "autoColor-" + i;
+          graph.lineColorField = colorKey;
+          graph.fillColorsField = colorKey;
+          for (var x = 0; x < chart.dataProvider.length; x++) {
+            var color = ['#ff0000', '#ff8000', '#ffff00', '#40ff00', '#33cccc', '#339933', '#ff33cc', '#00bfff', '#ffcc66', '#00cc00', '#0066ff', '#ffcc00', '#ff6666'][x];
+            
+            chart.dataProvider[x][colorKey] = color;
+          }
+        }
+
+      }, ["serial"]);
+
+      var chart = new AmCharts.AmSerialChart();
+      chart.dataProvider = chartData;
+      chart.categoryField = "company_name";
+      //      chart.startDuration = 1;
+      chart.startEffect = "elastic ";
+      chart.theme = 'light';
+
+      chart.titles = [{
+        "text": "Cumulative returns for each stock",
+        "bold": true,
+        }];
+
+      // add click listener
+      chart.addListener("clickGraphItem", handleClick);
+
+
+      // AXES
+      // category
+      var categoryAxis = chart.categoryAxis;
+      categoryAxis.labelRotation = 90;
+      categoryAxis.gridPosition = "start";
+      categoryAxis.title = "Companies";
+      categoryAxis.titleColor = 'grey';
+
+      // value
+      // in case you don't want to change default settings of value axis,
+      // you don't need to create it, as one value axis is created automatically.
+      var valueAxis = new AmCharts.ValueAxis();
+      valueAxis.title = "Cumulative Return (%)";
+      valueAxis.titleColor = 'grey';
+      chart.addValueAxis(valueAxis);
+      
+      
+      // GRAPH
+      var graph = new AmCharts.AmGraph();
+      graph.valueField = "last_cr";
+      graph.balloonText = "[[category]]: [[value]]";
+      graph.type = "column";
+      graph.lineAlpha = 0;
+      graph.fillAlphas = 0.8;
+      chart.rotate = true;
+      chart.columnWidth = 1;
+      graph.autoColor = true;
+
+      chart.addGraph(graph);
+
+      chart.write("chartdiv2");
     }
   }
 };
