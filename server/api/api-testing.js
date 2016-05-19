0ed72655a97c3b785d1bf0f1f018653b286242c5
Kelvin Li <kelvinlcf@yahoo.com.hk>
2016-05-19 18:16:01 +1000

    reupload

--- server/api/api-testing.js
+++ server/api/api-testing.js
@@ -9,47 +9,45 @@ APItesting = {
 
 
       var token = params['token'];
-      var company = "AAC.AX";
+      var company_name = "AAC.AX";
       var topic = "Cash Rate";
-
       var lower_range = -5;
       var upper_range = 5;
 
-
+      var stocks = StockPrices.find({company_name: company_name, token:token}, {fields: {'date':1, 'cum_return':1}}).fetch();
       var chartData = [];
-      var graphs = [];
-      var dates = _.uniq(StockEvents.find({token:token,company_name: company, topic: topic, value: {$gt: 0}},{sort:{date:1},fields: {date: true}}).fetch().map(function(x){return x.date}),true);
+      var guides = [];
 
-      for (var date = lower_range; date <= upper_range; date++) {
-        var entry = {
-          year: date,
+      // events
+      var events = StockEvents.find({token: token, company_name: company_name, topic: topic, value: {$gt : 0}}, {fields: {'date':1}}).fetch(); 
+      // console.log(events);
+
+      events.forEach(function(c) {
+        var dateLower = new Date(c.date);
+        dateLower.setDate(dateLower.getDate() + lower_range);
+        var dateUpper = new Date(c.date);
+        dateUpper.setDate(dateUpper.getDate() + upper_range);
+
+        // console.log(c.date);
+        // console.log(dateLower);
+        // console.log(dateUpper);
+        var significantDays = StockPrices.find({company_name: company_name, token:token, date: {$gte : dateLower, $lte : dateUpper}}, {fields: {'cum_return':1}, sort: {'cum_return':1}}).fetch();
+        console.log(significantDays);
+        if (significantDays != undefined) {
+          var hi = significantDays[Object.keys(significantDays)[Object.keys(significantDays).length - 1]].cum_return;
+          var lo = significantDays[Object.keys(significantDays)[0]].cum_return;
+          // console.log(hi);
+          // console.log(lo);
+          //fruitObject[Object.keys(fruitObject)[Object.keys(fruitObject).length - 1]] 
+          var p = (hi - lo);
+          console.log(p);
+
+          var significance = "Non-significant event";
         }
-        dates.forEach(function (d) {
-          var currDate = new Date(d.getTime());
-          currDate.setDate(d.getDate()+date);
-          var cr = StockPrices.findOne({token: token, company_name: company, date: currDate},{fields:{cum_return:true}});
-          if (cr === undefined)
-            cr = null;
-          else
-            cr = cr.cum_return;
-          entry[d.toDateString()] = cr;
-        });
-
-        chartData.push(entry);
-      }
-
-      dates.forEach(function(d){
-        graphs.push({
-          "balloonText": "cumulative return  is [[value]] at [[category]] day relative to "+d.toDateString(),
-          "bullet": "round",
-          "hidden": false,
-          "title": d.toDateString(),
-          "valueField": d.toDateString(),
-          "fillAlphas": 0,
-        });  
       });
 
-      API.utility.response(context, 200, chartData);
+
+      API.utility.response(context, 200, {});
 
 
 
